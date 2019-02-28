6c6e485fb8fc747dabff5a0c8cfeffe89a3a10b9
Davain Pablo Edwards <core8@gmx.net>
2019-02-28 12:22:50 +0100

    :star: add consts and attributes to proxy logic class

--- ProxyServer/ProxyServer/ProxyThread.cs
+++ ProxyServer/ProxyServer/ProxyThread.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -16,6 +17,21 @@ namespace ProxyServer
     */
     public class ProxyThread
     {
+
+        public int ExternalPort { get; set; }
+        public int InternalPort { get; set; }
+        public bool RewriteHostHeaders { get; set; }
+        public bool Stopped { get; set; }
+
+        private TcpListener Listener { get; set; }
+
+        private readonly long PROXY_TIMEOUT_TICKS = new TimeSpan(0, 1, 0).Ticks;
+
+        private const string HTTP_SEPARATOR = "\r\n";
+        private const string HTTP_HEADER_BREAK = HTTP_SEPARATOR + HTTP_SEPARATOR;
+
+        private readonly string[] HTTP_SEPARATORS = new string[] { HTTP_SEPARATOR };
+        private readonly string[] HTTP_HEADER_BREAKS = new string[] { HTTP_HEADER_BREAK };
 
 
 
