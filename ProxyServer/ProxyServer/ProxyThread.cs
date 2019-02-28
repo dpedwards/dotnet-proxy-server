668fed8cc610f797463b07f3b8cbdd0fddda6eab
Davain Pablo Edwards <core8@gmx.net>
2019-02-28 12:19:46 +0100

    :star: add proxy logic class and initial some consts and attributes

---
+++ ProxyServer/ProxyServer/ProxyThread.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProxyServer
+{
+
+   /**
+    * Used to define proxy logic
+    * 
+    * @author Davain Pablo Edwards
+    * @license MIT 
+    * @version 1.0
+    */
+    public class ProxyThread
+    {
+
+
+
+    }
+}
--- ProxyServer/ProxyServer/frmMain.cs
+++ ProxyServer/ProxyServer/frmMain.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,8 +11,26 @@ using System.Windows.Forms;
 
 namespace ProxyServer
 {
+
+    /**
+    * Used to define app interaction logic
+    * 
+    * @author Davain Pablo Edwards
+    * @license MIT 
+    * @version 1.0
+    */
     public partial class frmMain : Form
     {
+        // Initial port span 
+        private const int MIN_PORT = 1;
+        private const int MAX_PORT = 65535;
+
+        public static readonly string CommonDataPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData), "ProxyProxy");
+        public static readonly string ConfigInfoPath = Path.Combine(CommonDataPath, "config.txt");
+
+        private ProxyThread ProxyThreadListener = null;
+
+
         public frmMain()
         {
             InitializeComponent();
