4be0b26022da007de054ea35b52a99fcf9c54076
Davain Pablo Edwards <core8@gmx.net>
2019-02-28 12:07:35 +0100

    :up: update button design names

--- ProxyServer/ProxyServer/frmMain.Designer.cs
+++ ProxyServer/ProxyServer/frmMain.Designer.cs
@@ -28,24 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.cmbIPAddress = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtExternalPort = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.txtInternalPort = new System.Windows.Forms.TextBox();
+            this.chkRewriteHostHeaders = new System.Windows.Forms.CheckBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // comboBox1
+            // cmbIPAddress
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(12, 25);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(188, 21);
-            this.comboBox1.TabIndex = 0;
+            this.cmbIPAddress.FormattingEnabled = true;
+            this.cmbIPAddress.Location = new System.Drawing.Point(12, 25);
+            this.cmbIPAddress.Name = "cmbIPAddress";
+            this.cmbIPAddress.Size = new System.Drawing.Size(188, 21);
+            this.cmbIPAddress.TabIndex = 0;
             // 
             // label1
             // 
@@ -65,12 +65,12 @@
             this.label2.TabIndex = 2;
             this.label2.Text = "External Port";
             // 
-            // textBox1
+            // txtExternalPort
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 65);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(188, 20);
-            this.textBox1.TabIndex = 3;
+            this.txtExternalPort.Location = new System.Drawing.Point(12, 65);
+            this.txtExternalPort.Name = "txtExternalPort";
+            this.txtExternalPort.Size = new System.Drawing.Size(188, 20);
+            this.txtExternalPort.TabIndex = 3;
             // 
             // label3
             // 
@@ -81,55 +81,55 @@
             this.label3.TabIndex = 4;
             this.label3.Text = "Internal Port";
             // 
-            // textBox2
+            // txtInternalPort
             // 
-            this.textBox2.Location = new System.Drawing.Point(12, 104);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(188, 20);
-            this.textBox2.TabIndex = 5;
+            this.txtInternalPort.Location = new System.Drawing.Point(12, 104);
+            this.txtInternalPort.Name = "txtInternalPort";
+            this.txtInternalPort.Size = new System.Drawing.Size(188, 20);
+            this.txtInternalPort.TabIndex = 5;
             // 
-            // checkBox1
+            // chkRewriteHostHeaders
             // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(12, 130);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(126, 17);
-            this.checkBox1.TabIndex = 6;
-            this.checkBox1.Text = "Rewrite host headers";
-            this.checkBox1.UseVisualStyleBackColor = true;
+            this.chkRewriteHostHeaders.AutoSize = true;
+            this.chkRewriteHostHeaders.Location = new System.Drawing.Point(12, 130);
+            this.chkRewriteHostHeaders.Name = "chkRewriteHostHeaders";
+            this.chkRewriteHostHeaders.Size = new System.Drawing.Size(126, 17);
+            this.chkRewriteHostHeaders.TabIndex = 6;
+            this.chkRewriteHostHeaders.Text = "Rewrite host headers";
+            this.chkRewriteHostHeaders.UseVisualStyleBackColor = true;
             // 
-            // button1
+            // btnStart
             // 
-            this.button1.Location = new System.Drawing.Point(12, 153);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 7;
-            this.button1.Text = "Start";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnStart.Location = new System.Drawing.Point(12, 153);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 7;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // btnStop
             // 
-            this.button2.Location = new System.Drawing.Point(125, 153);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 8;
-            this.button2.Text = "Stop";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btnStop.Location = new System.Drawing.Point(125, 153);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 8;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
             // 
             // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(212, 185);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.checkBox1);
-            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.chkRewriteHostHeaders);
+            this.Controls.Add(this.txtInternalPort);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtExternalPort);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.cmbIPAddress);
             this.Name = "frmMain";
             this.Text = "ProxyServer";
             this.Load += new System.EventHandler(this.frmMain_Load);
@@ -140,15 +140,15 @@
 
         #endregion
 
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox cmbIPAddress;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtExternalPort;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.CheckBox checkBox1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txtInternalPort;
+        private System.Windows.Forms.CheckBox chkRewriteHostHeaders;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
     }
 }
 
