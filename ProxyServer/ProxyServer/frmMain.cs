9b7cd357f12b307191dabc58b70270ad1dd5ecbf
Davain Pablo Edwards <core8@gmx.net>
2019-02-28 15:19:49 +0100

    :star: add function to get local IP address

--- ProxyServer/ProxyServer/frmMain.cs
+++ ProxyServer/ProxyServer/frmMain.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -34,6 +35,67 @@ namespace ProxyServer
         public frmMain()
         {
             InitializeComponent();
+
+            this.Text += " " + System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString();
+
+            var ips = GetLocalIPs().OrderBy(x => x);
+            if (ips.Any())
+            {
+                cmbIPAddress.Items.Clear();
+                foreach (string ip in ips)
+                {
+                    cmbIPAddress.Items.Add(ip);
+                }
+                cmbIPAddress.Text = cmbIPAddress.Items[0].ToString();
+            }
+
+            int port = 5000; // listen on external port 
+            while (!CheckPortAvailability(port))
+            {
+                port++;
+            }
+            txtExternalPort.Text = port.ToString();
+        }
+
+        /*
+         * GetLocalIPs() Function to get local IP address
+         * 
+         * @return ownIPS
+         */ 
+        private List<string> GetLocalIPs()
+        {
+            // Try to find our internal IP address...
+            string myHost = System.Net.Dns.GetHostName();
+            IPAddress[] addresses = System.Net.Dns.GetHostEntry(myHost).AddressList;
+            List<string> ownIPs = new List<string>();
+            string fallbackIP = "";
+
+            for (int i = 0; i < addresses.Length; i++)
+            {
+                // Is this a valid IPv4 address?
+                if (addresses[i].AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+                {
+                    string thisAddress = addresses[i].ToString();
+                    
+                    // Loopback is not our preference...
+                    if (thisAddress == "127.0.0.1")
+                        continue;
+
+                    // 192.x.x.x addresses are self-assigned "private network" IP by Windows
+                    if (thisAddress.StartsWith("192"))
+                    {
+                        fallbackIP = thisAddress;
+                        continue;
+                    }
+                    ownIPs.Add(thisAddress);
+                }
+            }
+            if (ownIPs.Count == 0 && !string.IsNullOrEmpty(fallbackIP))
+            {
+                ownIPs.Add(fallbackIP);
+            }
+
+            return ownIPs;
         }
 
         private void frmMain_Load(object sender, EventArgs e)
